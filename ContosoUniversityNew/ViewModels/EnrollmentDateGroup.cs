ed1068fc60fa4af6843783a1eb9087610789a41a
Chris_Jarvis <clj@ollure.com>
2015-10-30 16:12:29 -0500

    errors ons ection three

---
+++ ContosoUniversityNew/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversityNew.ViewsModels
+{
+    public class EnrollmentDataGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
